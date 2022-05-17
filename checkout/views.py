35155ad35ea11b0618ba9b050e95e2ccc20091f7
Magda <madeline@gmx.co.uk>
2022-05-17 19:03:51 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Ko6lqGKBs0lKX6JPEr3izbN2GXiWzjJ9LYPi5A0XwQ7Dfs5oP790LBFG3vMCTiun3NCftEMZUR8UDdQuMuDGLXd00ElD2kwwK',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
