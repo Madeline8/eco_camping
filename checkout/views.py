52dfafe902d32bb4a2c628ba15936f3fa615ef6b
Magda <madeline@gmx.co.uk>
2022-05-18 17:04:49 +0000

    Added checkout form data caching in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -8,6 +9,24 @@ from products.models import Product
 from bag.contexts import bag_contents
 
 import stripe
+import json
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -33,9 +52,7 @@ def checkout(request):
             order = order_form.save()
             for item_id, item_data in bag.items():
                 try:
-                    # Get the product id out of the bag
                     product = Product.objects.get(id=item_id)
-                    # for product with no size
                     if isinstance(item_data, int):
                         order_line_item = OrderLineItem(
                             order=order,
@@ -44,7 +61,6 @@ def checkout(request):
                         )
                         order_line_item.save()
                     else:
-                        # for products with size
                         for size, quantity in item_data['items_by_size'].items():
                             order_line_item = OrderLineItem(
                                 order=order,
@@ -53,16 +69,14 @@ def checkout(request):
                                 product_size=size,
                             )
                             order_line_item.save()
-                # What hapens when product isn't found 
                 except Product.DoesNotExist:
                     messages.error(request, (
                         "One of the products in your bag wasn't found in our database. "
                         "Please call us for assistance!")
                     )
-                    # Delete empty order 
                     order.delete()
                     return redirect(reverse('view_bag'))
-            # Save profile information to the session 
+
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
