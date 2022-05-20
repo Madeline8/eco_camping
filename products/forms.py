77376b33f9669faa0c9a4f8c03ed43c1477ad5fb
Magda <madeline@gmx.co.uk>
2022-05-20 19:52:45 +0000

    Product Admin - Product Form

---
+++ products/forms.py
@@ -0,0 +1,20 @@
+
+from django import forms
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        # Create list of tuples of friendly names associated with categories' id's  - list comprehension
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'rounded-0'
