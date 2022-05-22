dd75d1777326fbc6905ea189c9bff948fe38245e
Magda <madeline@gmx.co.uk>
2022-05-22 21:50:06 +0000

    Added Contact Model and customised Admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    model = Contact
+    list_display = (
+        'full_name', 'email', 'subject', 'message'
+    )
+
+    ordering = ('full_name',)
+
+admin.site.register(Contact, ContactAdmin)
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    """Model to send a message and keep it on the database"""
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=80, null=False, blank=False)
+    subject = models.CharField(max_length=50, null=False, blank=False)
+    message = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.subject
