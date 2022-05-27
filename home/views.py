de30edcf370d12a1b1eaa67bbcf204f6a36d8b5b
Magda <madeline@gmx.co.uk>
2022-05-27 19:02:18 +0000

    Company policies - with collapse plugins

--- home/views.py
+++ home/views.py
@@ -5,3 +5,10 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def policies(request):
+    """
+    A view to return the company policies
+    """
+    return render(request, 'home/policies.html')
